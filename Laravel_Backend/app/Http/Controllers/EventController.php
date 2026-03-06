689a706bdf6297de17d094e621e677ddbca37b72
Ricsi <thereallemonmail@gmail.com>
2026-03-07 00:48:02 +0100

    migrations added, api routes created, crud functions, models

---
+++ Laravel_Backend/app/Http/Controllers/EventController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    public function index(Request $request)
+    {
+        $events = Event::where('user_id', $request->user()->id)->get();
+
+        return response()->json($events);
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'occurrence' => 'required|date',
+            'description' => 'nullable|string'
+        ]);
+        $newEvent = Event::create([
+            'user_id' => $request->user()->id,
+            'title' => $request->title,
+            'occurrence' => $request->occurrence,
+            'description' => $request->description
+        ]);
+        return response()->json($newEvent, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+            $request->validate([
+            'title' => 'required|string|max:255',
+            'occurrence' => 'required|date',
+            'description' => 'nullable|string'
+        ]);
+        $event = Event::where('id', $id)->where('user_id', $request->user()->id)-first();
+
+        if(!$event)
+        {
+            return response()->json([
+                'message' => "Event not found"
+            ], 404);
+        }
+        $event->update([
+            'title' => $request->title,
+            'occurrence' => $request->occurrence,
+            'description' => $request->description
+        ]);
+        
+        return response()->json($event);
+    }
+
+
+    public function destroy(Request $request, $id)
+    {
+        $event = Event::where('id', $id)->where('user_id', $request->user()->id)->first();
+
+        if(!$event)
+        {
+            return response()->json([
+                'message' => "Event not found"
+            ], 404);
+        }
+        $event->delete();
+        return response()->json([
+            'message' => 'Event deleted'
+        ], 200);
+    }
+}
--- Laravel_Backend/app/Models/User.php
+++ Laravel_Backend/app/Models/User.php
@@ -6,11 +6,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Event;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+
+
 
     /**
      * The attributes that are mass assignable.
